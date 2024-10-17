1208d10cee4b028cabb5dc3d5445533f41cd30d0
ThuyTrang9525 <buithithuytrang0905@gmail.com>
2024-10-17 13:36:34 +0700

    17/10

---
+++ array.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    //$color = array("red","green","blue");
+    
+    /*foreach ($color as $x) {
+        echo "$x <br>";
+    }*/
+    /*for ($i=0; $i< Count($color);$i++){
+        echo $color[$i];
+        echo "<br>";
+    }*/
+    /*for ($i=0; $i< Count($color);$i++){
+        echo "<p style='color:".$color[$i].";font-size:30px'>".$color[$i]."</p>";
+    }*/
+    // mảng kết hợp
+        /*$course= array(
+            'FRONT_END' => array(
+                'title'=> "Khóa học lập trình Frontend online",
+                'fee' => "1234567"
+            ),
+            'MYSQL' => array(
+                'title'=> "Khóa học lập trình web PHP MYSQL",
+                'fee' => "654654"
+            ) 
+        );
+        foreach($course as $k=>$v){
+            echo "{$k} <br>";
+            echo "--{$v['title']}<br>";
+            echo "--{$v['fee']}<br>";
+            echo "<br>";
+        }*/
+
+
+
+    //school
+    /*$school = array(
+        'Student' => array(
+            array(
+                'masv' => "1",
+                'ten' => "Trang",
+                'ngay sinh' => "2005",
+                'gioi tinh' => "nu"
+            ),
+            array(
+                'masv' => "2",
+                'ten' => "Lan",
+                'ngay sinh' => "2006",
+                'gioi tinh' => "nu"
+            ),
+            array(
+                'masv' => "3",
+                'ten' => "Minh",
+                'ngay sinh' => "2004",
+                'gioi tinh' => "nam"
+            )
+        ),
+        'Teacher' => array(
+            array(
+                'magv' => "1",
+                'ten' => "Thầy An",
+                'ngay sinh' => "1980",
+                'gioi tinh' => "nam"
+            ),
+            array(
+                'magv' => "2",
+                'ten' => "Cô Bình",
+                'ngay sinh' => "1985",
+                'gioi tinh' => "nu"
+            ),
+            array(
+                'magv' => "3",
+                'ten' => "Thầy Châu",
+                'ngay sinh' => "1978",
+                'gioi tinh' => "nam"
+            )
+        )
+    );*/
+     /*foreach ($school as $k => $v) {
+        echo "{$k} <br>";
+        foreach ($v as $person) {
+            if ($k == 'Student') {
+                echo "--Mã SV: {$person['masv']}<br>";
+            } else {
+                echo "--Mã GV: {$person['magv']}<br>";
+            }
+            echo "--Tên: {$person['ten']}<br>";
+            echo "--Ngày sinh: {$person['ngay sinh']}<br>";
+            echo "--Giới tính: {$person['gioi tinh']}<br>";
+            echo "<br>";
+        }
+    }*/
+
+    
+    ?>
+</body>
+</html>
